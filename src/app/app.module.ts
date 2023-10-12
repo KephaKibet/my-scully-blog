43461e2ad43dea5e5763db291f0696733c6c5f4c
Kepha Kibet <kibet.kepha@live.com>
2023-10-12 22:33:57 +0300

    add article module and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
